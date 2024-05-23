400c84cc626c144a587849990da2d3f66979ea93
Veit Gemmer <gemv@viessmann.com>
2024-05-23 21:38:50 +0200

    added equipment

---
+++ equipment_entry.py
@@ -0,0 +1,25 @@
+class EquipmentEntry():
+
+    def __init__(self, description='None', quantity=0):
+        self._description = description
+        self._quantity = quantity
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description: str):
+        self._description = description
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, quantity: int):
+        self.quantity = quantity
+
+    def as_str(self):
+        entry_str = f'{self._quantity}x {self._description}'
+        return entry_str
---
+++ equipment_register.py
@@ -0,0 +1,20 @@
+# sound
+top_small = r'12'' Fullrange Lautsprecher'
+top_big = r'15'' Fullrange Lautsprecher'
+sub = r'Doppel 12'' Subwoofer'
+mic_cable = 'Mikrofon (Kabel)'
+mic_wireless = 'Mikrofon (Funk)'
+
+# light
+ali_pair = '18x18W RGBWAUV Uplight IP 65'
+led_bar = 'LED Bar'
+superfly  = '2in1 Derby-Effekt & Strobe-LED Superfly'
+light_pole = 'Lichtstativ T-Träger'
+multi_fx_bar = 'Multi-FX Laser Bar'
+light_mixer = 'Lichtmischpult'
+traverse_pole = 'Traversenstativ (max. Höhe: 4m)'
+traverse_mid = '3m Traverse'
+traverse_big = '4,5m Traverse'
+laser = 'RGY Laser (Klasse 3R)'
+moving_head = 'LED Moving Head'
+strobe = 'Stroboskop'
